bffa2e4083ee80f4771b42ff8669d8c36cde537a
lilrain628 <vova.melentif@yandex.ru>
2026-03-06 23:13:36 +0700

    complete week3-4

--- tools/check_tests.py
+++ tools/check_tests.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 def main():
-    group = os.getenv("GROUP", "431")
-    sid = os.getenv("STUDENT_ID", "s01")
+    group = os.getenv("GROUP", "332")
+    sid = os.getenv("STUDENT_ID", "s10")
     
     print(f"Checking tests for {group}/{sid}...")
     
